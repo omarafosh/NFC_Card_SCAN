b5e8ca101a3cbba114fba6d2739abdff5c398c79
Omar Afosh <omarafosh@gmail.com>
2025-12-19 16:13:26 +0200

    Update version to v1.1 and trigger Vercel build

--- src/app/page.js
+++ src/app/page.js
@@ -30,7 +30,7 @@ export default function Home() {
             <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-indigo-400 opacity-75"></span>
             <span className="relative inline-flex h-2 w-2 rounded-full bg-indigo-500"></span>
           </span>
-          v1.0 Now Available
+          v1.1 Now Available
         </div>
 
         <h1 className="max-w-4xl text-5xl font-extrabold tracking-tight sm:text-7xl">
